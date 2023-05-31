4d6e76aec6fa47e834c44a5627d23dccba719118
Javez <mr.javez@ukr.net>
2023-05-31 14:44:02 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navigation from "./layout/Navigation";
+
+class App extends React.Component {
+  render() {
+    return <Navigation />;
+  }
+}
+
+export default App;
